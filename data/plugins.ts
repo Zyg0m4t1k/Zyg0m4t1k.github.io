1f0eb387f2cc73c38e6b7f20c22ba13ba48f3347
Zyg0m4t1k <l4ss4t3r@gmail.com>
2026-06-13 15:08:34 +0200

    docs: add icon plugin documentation and entry in plugins catalogue

--- data/plugins.ts
+++ data/plugins.ts
@@ -235,7 +235,7 @@ const plugins: PluginItem[] = [
    {
     "id": "whatwatt",
     "name": "Whattwat",
-    "description": "Ce plugin permet d'intégrer le compteur d’énergie connecté WhatWatt dans Jeedom pour suivre consommation, production, tension, courant et énergie électrique.",
+    "description": "Ce plugin permet d’intégrer le compteur d’énergie connecté WhatWatt dans Jeedom pour suivre consommation, production, tension, courant et énergie électrique.",
     "icon": "whatwatt_icon.png",
     "badges": ["Jeedom 4.4+", "beta", "os 11+", "php 7.4/8.x","Node.js"],
     "routes": {
@@ -245,10 +245,24 @@ const plugins: PluginItem[] = [
       "github": "https://github.com/Zyg0m4t1k/",
       "market": ""
     }
+  },
+  {
+    "id": "icon",
+    "name": "Plugin Icônes",
+    "description": "Ajoutez, installez, désinstallez et prévisualisez des packs d’icônes personnalisés dans Jeedom.",
+    "icon": "icon_icon.png",
+    "badges": ["Jeedom 4.4+", "beta", "os 11+", "php 7.4/8.x"],
+    "routes": {
+      "page": "/docs/plugins/icon/"
+    },
+    "links": {
+      "github": "https://github.com/Zyg0m4t1k/",
+      "market": "https://market.jeedom.com/index.php?v=d&p=market_display&id=3328"
+    }
   }
-  
-  
-  
+
+
+
 ];
 
 export default plugins;
